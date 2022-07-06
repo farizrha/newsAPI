e39977e659d2d05a16f457c95db675c3342bbb50
farizrha <farizhaykal1@gmail.com>
2022-07-06 18:26:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    {/* Navbar */}
+    <nav className="navbar navbar-light" style={{backgroundColor: '#6afff3'}}>
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">News Web App</span>
+      </div>
+    </nav>
+    <main>
+      <section className="py-5 text-center container">
+        <div className="row py-lg-5">
+          <div className="col-lg-6 col-md-8 mx-auto">
+            <h1 className="fw-light p-3">Find your latest news</h1>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </section>
+      <div className="album py-5 bg-light">
+        <div className="container">
+          <div id="news" className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">  
+           
+          </div>
+        </div>
+      </div>
+    </main>
+
+    
+  </div>
+
+  
+
   );
 }
 
